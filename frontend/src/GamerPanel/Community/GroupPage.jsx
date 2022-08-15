d125edbad6c353cd685324d272be9d28314f84d8
Rania Saadi <88983972+raniasaadi16@users.noreply.github.com>
2022-08-15 20:41:29 +0200

    update urls

--- frontend/src/GamerPanel/Community/GroupPage.jsx
+++ frontend/src/GamerPanel/Community/GroupPage.jsx
@@ -13,7 +13,7 @@ import { GroupPost } from './GroupPost'
 import { io } from 'socket.io-client';
 import Interweave from 'interweave'
 
-let socket = io('http://31.187.72.239:8000');
+let socket = io('https://api.egorgaming.com/api');
 
 
 export const GroupPage = () => {

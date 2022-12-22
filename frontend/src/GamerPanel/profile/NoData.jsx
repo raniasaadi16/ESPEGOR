b22798764d0677fd7cf39427bcd8b9ff7d2a9028
raniasaadi16 <saadirania406@gmail.com>
2022-12-22 21:01:21 +0000

    edit server

--- frontend/src/GamerPanel/profile/NoData.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-export const NoData = (props) => {
-    return (
-        <div className="f-c-c no-data">
-            <span>{props.msg}</span>
-        </div>
-    )
-}

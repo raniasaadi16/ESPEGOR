8bfd863db01c9dfcef0672c35f179c0f537d28e1
Rania Saadi <88983972+raniasaadi16@users.noreply.github.com>
2023-01-06 20:27:15 +0100

    fix admin isuu

--- backend/App/utils/cloudinary.js
+++ backend/App/utils/cloudinary.js
@@ -3,9 +3,9 @@ require('dotenv').config({path: __dirname + '*/**/.env'});
 
 //console.log(process.env.CLOUDINARY_KEY, __dirname)
 cloudinary.config({
-  cloud_name: process.env.CLOUDINARY_HOST,
-  api_key: process.env.CLOUDINARY_KEY,
-  api_secret: process.env.CLOUDINARY_SECRERT,
+  cloud_name: 'ddu6qxlpy',
+  api_key: '633878313745772',
+  api_secret: 'hSeciRWSeYzda3-T9-adPNKiElE',
 });
 
 module.exports = cloudinary; 
